abf3b03b26ad5b5ecdc3124af8441303234e97ee
WaYease <yu_weiyi@outlook.com>
2024-07-31 19:11:57 +0800

    build: init project

---
+++ src/main/java/indi/wayease/linuxdo/wiki/backend/monolith/LinuxDoWikiBackendApplication.java
@@ -0,0 +1,13 @@
+package indi.wayease.linuxdo.wiki.backend.monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxDoWikiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxDoWikiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/indi/wayease/linuxdo/wiki/backend/monolith/LinuxDoWikiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.wayease.linuxdo.wiki.backend.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxDoWikiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
